95bc33db05c65eda9391b542f07951c08f9cf9cd
eder6 <eder@b2mlportal.com.br>
2021-08-11 13:14:56 -0300

    First commit

---
+++ android/app/src/main/kotlin/com/example/contator_de_pessoas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.contator_de_pessoas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/widgets.dart';
+
+
+import 'package:flutter/material.dart';
+
+void main(){
+
+runApp(MaterialApp(
+
+  title: "contador de Pessoas",
+  home: Container(color: Colors.white,),
+));
+
+}
