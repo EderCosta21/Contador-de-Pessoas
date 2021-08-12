5dcbdf88ccf0004d7a3aae8e348fa3f04d682e39
eder6 <eder@b2mlportal.com.br>
2021-08-11 23:45:17 -0300

    Atualização Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,100 @@
-import 'package:flutter/widgets.dart';
-
-
 import 'package:flutter/material.dart';
 
-void main(){
+void main() {
+  runApp(MaterialApp(title: "contador de Pessoas", home: Home()));
+}
 
-runApp(MaterialApp(
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
 
-  title: "contador de Pessoas",
-  home: Container(color: Colors.white,),
-));
+class _HomeState extends State<Home> {
+  int _people = 0;
 
+  String _infTexto = "Pode Entrar ";
+
+  void _changePeople(int value) {
+    setState(() {
+      _people += value;
+
+      if(_people <0 ) {
+        _infTexto = "Erro no contador";
+      }else if ( _people < 10 )
+      {
+        _infTexto = "Pode Entrar ";
+      }
+      else {
+        _infTexto = "Lotado! ";
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: <Widget>[
+        Image.asset(
+          "images/restaurant.jpg",
+          fit: BoxFit.cover,
+          height: 10000.0,
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              "Pessoas $_people",
+              style:
+                  TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: TextButton(
+                    child: Text(
+                      "+1",
+                      style: TextStyle(
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(1);
+
+                      debugPrint("+1");
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.all(12.0),
+                  child: TextButton(
+                    child: Text(
+                      "-1",
+                      style: TextStyle(
+                          fontSize: 40.0,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    onPressed: () {
+                      _changePeople(-1);
+                      debugPrint("-1");
+                    },
+                  ),
+                )
+              ],
+            ),
+            Text(
+              "$_infTexto",
+              style: TextStyle(
+                  color: Colors.white,
+                  fontStyle: FontStyle.italic,
+                  fontSize: 20.0),
+            ),
+          ],
+        )
+      ],
+    );
+  }
 }
